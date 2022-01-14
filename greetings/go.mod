c1f2000c5a4d7d4a65e84576784278f2baad13f6
seacoastcoder <37166400+seacoastcoder@users.noreply.github.com>
2022-01-13 23:09:16 -0500

    Make a local module for use, and make changes necessary to run

---
+++ greetings/go.mod
@@ -0,0 +1,3 @@
+module seacoastcoder.com/greetings
+
+go 1.17
--- hello/hello.go
+++ hello/hello.go
@@ -1,10 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"seacoastcoder.com/greetings"
+)
 
 import "rsc.io/quote"
 
 func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println(quote.Go())
+
+	message := greetings.Hello("Test")
+	fmt.Println(message)
 }
